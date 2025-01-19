ba6aba2d4752d3b9218ed530ffff142685895924
Akshat-palia <akshatpalia@gmail.com>
2025-01-19 14:16:18 +0530

    keep both

--- list.py
+++ list.py
@@ -2,5 +2,5 @@ print("here is the list in python")
 lang=["c","java","python"]
 print("some of the programming languages are ",lang)
 print('this is a new feature(in no other lang)')
-print('this is a new feature(in python)')
+
 
