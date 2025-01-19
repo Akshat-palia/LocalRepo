be078a0faab7edd9e6277958508f32ea65d84b16
Akshat-palia <akshatpalia@gmail.com>
2025-01-19 12:28:46 +0530

    Specified lang

--- list.py
+++ list.py
@@ -1,4 +1,4 @@
 print("here is the list in python")
 lang=["c","java","python"]
 print("some of the programming languages are ",lang)
-print('this is a new feature')
+print('this is a new feature(in python)')
