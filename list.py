45a6f4137af2502b0e6429a4ffd552a8dfbd5eaa
Akshat-palia <akshatpalia@gmail.com>
2025-01-19 13:58:21 +0530

    add both features

--- list.py
+++ list.py
@@ -1,8 +1,6 @@
 print("here is the list in python")
 lang=["c","java","python"]
 print("some of the programming languages are ",lang)
-<<<<<<< HEAD
 print('this is a new feature(in no other lang)')
-=======
 print('this is a new feature(in python)')
->>>>>>> main
+
